fee2d0ac6aca84d2b7331cf173cf37817465d204
Zilong Huang <speedinghzl@gmail.com>
2019-09-09 15:25:13 -0500

    init apex

--- run_local.sh
+++ run_local.sh
@@ -5,12 +5,16 @@ uname -a
 date
 
 CS_PATH=$1
+MODEL=$2
 LR=1e-2
 WD=5e-4
 BS=8
-STEPS=40000
+STEPS=$3
+INPUT_SIZE=$4
+OHEM=$5
 GPU_IDS=0,1,2,3
 
 #variable ${LOCAL_OUTPUT} dir can save data of you job, after exec it will be upload to hadoop_out path 
-python train.py --data-dir ${CS_PATH} --random-mirror --random-scale --restore-from ./dataset/resnet101-imagenet.pth --gpu ${GPU_IDS} --learning-rate ${LR}  --weight-decay ${WD} --batch-size ${BS} --num-steps ${STEPS}
-python evaluate.py --data-dir ${CS_PATH} --restore-from snapshots/CS_scenes_${STEPS}.pth --gpu 0
+CUDA_VISIBLE_DEVICES=${GPU_IDS} python -m torch.distributed.launch --nproc_per_node=4 train.py --data-dir ${CS_PATH} --model ${MODEL} --random-mirror --random-scale --restore-from ./dataset/resnet101-imagenet.pth --input-size ${INPUT_SIZE} --gpu ${GPU_IDS} --learning-rate ${LR}  --weight-decay ${WD} --batch-size ${BS} --num-steps ${STEPS} --ohem ${OHEM}
+# CUDA_VISIBLE_DEVICES=${GPU_IDS} python train.py --data-dir ${CS_PATH} --model ${MODEL} --random-mirror --random-scale --restore-from ./dataset/resnet101-imagenet.pth --gpu ${GPU_IDS} --learning-rate ${LR}  --weight-decay ${WD} --batch-size ${BS} --num-steps ${STEPS}
+CUDA_VISIBLE_DEVICES=${GPU_IDS} python -m torch.distributed.launch --nproc_per_node=4 evaluate.py --data-dir ${CS_PATH} --model ${MODEL} --input-size ${INPUT_SIZE} --batch-size 4 --restore-from snapshots/CS_scenes_${STEPS}.pth --gpu 0
